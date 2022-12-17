1b9d9ca5db48a2bae06f6e0c4bca7de1ada45a63
shaxruz25 <shaxruzismoilov250@gmail.com>
2022-12-17 22:01:42 +0500

    hero-banner media full

--- src/components/heroBanner/HeroBanner.jsx
+++ src/components/heroBanner/HeroBanner.jsx
@@ -1,7 +1,23 @@
 import './HeroBanner.css';
+// images import 
+import HeroBanner_images from '../../assets/images/hero_banner.png';
 const HeroBanner = () => {
     return (
-        <div>HeroBanner</div>
+        <div className='heroBanner'>
+            <div className="heroBanner-content">
+                <div className="heroBanner-left">
+                    <h1 className='heroBanner-title'>Our Best Mints Lip Balm Bundle</h1>
+                    <p className="heroBanner-text">Keep your lips magically moisturized and minty fresh with our best mints Lip Balm Bundle!</p>
+                    <div className='heroBanner-btn-group'>
+                        <button className="heroBanner-buy">Buy Now</button>
+                        <button className="heroBanner-explore">Explore Now</button>
+                    </div>
+                </div>
+                <div className="heroBanner-right">
+                    <img className='heroBanner-images' src={HeroBanner_images} alt="banner" />
+                </div>
+            </div>
+        </div>
     )
 }
 export default HeroBanner;
