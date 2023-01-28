70011959f4e08c6c0499c1432934034ba58b3123
shaxruz25 <shaxruzismoilov250@gmail.com>
2023-01-28 23:21:08 +0500

    cosmetics media full

--- src/components/conmetics/Cosmetics.jsx
+++ src/components/conmetics/Cosmetics.jsx
@@ -1,7 +1,24 @@
-import './Cosmetics.css';
+import "./Cosmetics.css";
+import Conmetics from "../../assets/images/conmetics.png";
 const Cosmetics = () => {
-    return (
-        <div>Cosmetics</div>
-    )
-}
+  return (
+    <div className="cosmetics">
+      <div className="cosmeticsis">
+        <span className="cosmetics_img">
+          <img src={Conmetics} alt="images" className="images" />
+        </span>
+        <div className="cosmetics_content">
+          <h2 className="cosmetics_title">
+            Women's Make Up, Lipsticks, Cosmetics, & Skin Care
+          </h2>
+          <p className="cosmetics_text">
+            This lip balm takes less than 30 mins to prepare, and you'll have a
+            blast doing it.
+          </p>
+          <button className="cosmetics_btn">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Cosmetics;
